74157aab98d04d173a9f2ad5376d6e91842118c0
SantiagoGomez1 <ibkyzz@gmail.com>
2022-09-12 17:25:03 -0300

    Backend finalizado, ya puedo pasar al frontend!

--- api/src/controllers/GET/getUser.js
+++ api/src/controllers/GET/getUser.js
@@ -3,11 +3,28 @@ const userSchema = require("../../models/user");
 const getUser = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await userSchema.findOne({ _id: id }).populate('publications',{
-      text: 1,
-      image: 1,
-      date: 1,
-    });
+    const user = await userSchema
+      .findOne({ _id: id })
+      .populate("publications", {
+        text: 1,
+        image: 1,
+        date: 1,
+      })
+      .populate("following", {
+        id: 1,
+        name: 1,
+        lastName: 1,
+        image: 1,
+        email: 1,
+      })
+      .populate("followers", {
+        id: 1,
+        name: 1,
+        lastName: 1,
+        image: 1,
+        email: 1,
+      });
+
     let userModified = {
       id: user.id,
       name: user.name,
@@ -15,6 +32,8 @@ const getUser = async (req, res) => {
       email: user.email,
       image: user.image,
       publications: user.publications,
+      followers: user.followers,
+      following: user.following,
     };
     if (!userModified) {
       throw new Error("Username does not exist");
